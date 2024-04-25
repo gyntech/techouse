7ad0b4b10a55b167c8f939ee2d2e5ce8065b11a1
gyntech <155608659+gyntech@users.noreply.github.com>
2024-04-25 17:43:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["You Are Welcome To Coded AB Technology", "Quality at its PEAK"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+var typed = new Typed(".multiple-text2", {
+    strings: ["You Are Welcome To TECH HOUSE", "Quality at its PEAK"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
